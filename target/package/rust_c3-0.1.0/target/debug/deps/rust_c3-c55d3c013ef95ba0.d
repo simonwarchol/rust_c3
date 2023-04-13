e05ddaff31928fdcf803e9ced051c4eac865fb03
Simon Warchol <simonwarchol@g.harvard.edu>
2023-04-12 21:05:08 -0400

    Adding crate

---
+++ target/package/rust_c3-0.1.0/target/debug/deps/rust_c3-c55d3c013ef95ba0.d
@@ -0,0 +1,11 @@
+/Users/swarchol/Research/rust_c3/target/debug/deps/rust_c3-c55d3c013ef95ba0.rmeta: src/lib.rs src/c3_color.rs src/c3_a.rs src/c3_t.rs src/c3_terms.rs
+
+/Users/swarchol/Research/rust_c3/target/debug/deps/librust_c3-c55d3c013ef95ba0.rlib: src/lib.rs src/c3_color.rs src/c3_a.rs src/c3_t.rs src/c3_terms.rs
+
+/Users/swarchol/Research/rust_c3/target/debug/deps/rust_c3-c55d3c013ef95ba0.d: src/lib.rs src/c3_color.rs src/c3_a.rs src/c3_t.rs src/c3_terms.rs
+
+src/lib.rs:
+src/c3_color.rs:
+src/c3_a.rs:
+src/c3_t.rs:
+src/c3_terms.rs:
---
+++ target/package/rust_c3-0.1.0/target/debug/deps/rust_c3-e28dfb68ff778d04.d
@@ -0,0 +1,11 @@
+/Users/swarchol/Research/rust_c3/target/debug/deps/rust_c3-e28dfb68ff778d04.rmeta: src/lib.rs src/c3_color.rs src/c3_a.rs src/c3_t.rs src/c3_terms.rs
+
+/Users/swarchol/Research/rust_c3/target/debug/deps/librust_c3-e28dfb68ff778d04.rlib: src/lib.rs src/c3_color.rs src/c3_a.rs src/c3_t.rs src/c3_terms.rs
+
+/Users/swarchol/Research/rust_c3/target/debug/deps/rust_c3-e28dfb68ff778d04.d: src/lib.rs src/c3_color.rs src/c3_a.rs src/c3_t.rs src/c3_terms.rs
+
+src/lib.rs:
+src/c3_color.rs:
+src/c3_a.rs:
+src/c3_t.rs:
+src/c3_terms.rs:
